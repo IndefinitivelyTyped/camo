ee9a499b5c68091e4094801b810fe2ac0469bce6
Max Battcher <me@worldmaker.net>
2016-02-04 22:51:19 -0500

    Explicitly reference directory index (fixes #1)
    Node (and Typescript in node module resolution mode) when asked to load a directory name will try loading an index file in that directory (index.js for Node; index.ts and then index.d.ts for Typescript in node resolution mode). Typings compiler doesn't current support this sort of resolution (see typings/typings#173). This is the easiest fix for the issue.

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
 import { connect } from './lib/db'
-import { getClient } from './lib/clients'
+import { getClient } from './lib/clients/index'
 
 import Document = require('./lib/document')
 import EmbeddedDocument = require('./lib/embedded-document')
